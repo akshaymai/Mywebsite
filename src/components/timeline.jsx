34441f30ac0d139a30d383ed29a3ea955bf36f79
akshay <akshay.maity76@gmail.com>
2019-12-27 18:35:57 +0530

    first commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>MERN Stack Developer at Gloify,Bangaluru<span>Nov,2019-Present</span></h2>
+                        <p>I have joined as a Software Developer in the Gloify team. During This Time I lern ReactJS,Html,Css,Bootstrap.</p>
+                      </div>
+                    </div>
+
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Trainee Software Developer at Balihans Software Pvt Ltd,Bangaluru <span>Dec,2018-Sept,2019</span></h2>
+                        <p>I have joined as a Software Developer-Trainee in the Balihans. During This Time ,My Responsibility is Backend-Development.I lern Javascript,NodeJs,Mongodb,MySQl .</p>
+                      </div>
+                    </div>
+                    
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Aksara Infotech <span>July,2018-Aug,2018</span></h2>
+                        <p>I have joined as a Software Developer-Intern in the Aksara team. During Intrenship Time I lern Core Java,Advance Java,SQL DataBase.</p>
+                      </div>
+                    </div>
+
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at NIT KOLKATA<span>2014-2018</span></h2>
+                        <p>I had done my under-graduation studies with major in IT.(Information Technology).</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Higher Secondary Education <span>2012-2014</span></h2>
+                        <p>I have completed my higher secondary education in Khukurdaha ICMM High School  with major subjects as Physics,Chemistry & Maths . During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Secondary Education <span>2011-2012</span></h2>
+                        <p>I have completed my secondary education in Gomokpota Gunadhar Vidyamandir . During my time at school, I have developed interest in Mathematics and Physics and Indian History </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
